6935702d159e43f9d9b9072c64338c73784dfe88
divashusaini <dishusaini2july@gmail.com>
2025-05-19 05:07:56 +0530

    Initial commit with fixed authentication

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl ="mongodb+srv://divanshuonwork:NgXP2URdFRs5HF8e@cluster0.s1osnsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbUrl =process.env.MONGODB_URI;
 
 main()
    .then(()=>{
